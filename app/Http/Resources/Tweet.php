25c92324b0c5721ea0fe1018421231c36649d71f
Martin C <martinchea@gmail.com>
2018-04-05 22:57:37 -0700

    added tweet model with show function

---
+++ app/Http/Resources/Tweet.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Tweet extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'content' => $this->content,
+            'user_slug' => $this->user->slug,
+            'user_full_name' => $this->user->name,
+            'created_at' => $this->created_at->toDateString(),
+        ];
+    }
+}
