af25fb4a221c1f558ddf1c2092c3aae7ffbb5c4a
Martin C <martinchea@gmail.com>
2018-04-05 21:57:17 -0700

    add tweet model and factory

---
+++ database/seeds/TweetTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TweetsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Tweet::class, 30)->create();
+    }
+}
