e9d6ac51acef7f0a3833958dc302f228d92f634a
Martin C <martinchea@gmail.com>
2018-06-03 02:28:39 -0700

    recommit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,7 +17,8 @@ Vue.use(require('vue-moment'));
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
-Vue.component('post-tweet-component', require('./components/PostTweetComponent.vue'));
+Vue.component('post-tweet-component', require('./components/PostTweet.vue'));
+Vue.component('get-tweets-component', require('./components/GetTweets.vue'));
 
 const app = new Vue({
     el: '#app'
