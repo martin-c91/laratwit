680875a5084d43216cda464b3d6b37e6d5a8ef12
Martin C <martinchea@gmail.com>
2018-06-25 04:27:46 -0700

    refactor .gitignore

---
+++ app/Http/Controllers/FollowingController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Following;
+use App\User;
+use Illuminate\Http\Request;
+use Auth;
+
+class FollowingController extends Controller
+{
+    public function __construct()
+    {
+        $this->user = User::find(request('user_id'));
+    }
+
+    public function store()
+    {
+        Auth::user()->followings()->syncWithoutDetaching($this->user->id);
+        return Auth::user()->followings;
+    }
+
+    public function destroy()
+    {
+        Auth::user()->followings()->detach($this->user->id);
+        return Auth::user()->followings;
+    }
+}
