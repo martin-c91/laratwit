b8b6e2ad7fa2d472f302174e794a6ff18ee10173
Martin C <martinchea@gmail.com>
2018-11-20 22:33:26 -0800

    added delete button and methods

--- resources/assets/js/components/GetTweets.vue
+++ resources/assets/js/components/GetTweets.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
-        <div class="panel" v-for="(tweet, index) in tweets">
-            <div class="panel-body mt-3 mb-3">
+        <div class="panel border-top rounded-top" v-for="(tweet, index) in tweets">
+            <div class="panel-body mt-3 mb-3 h-50">
                 <div style="float: left; width: 48px;">
                     <a :href="tweet.user.slug">
                         <img :src="tweet.user.avatar_url" class="avatar" alt="avatar">
                     </a>
                 </div>
                 <div style="margin-left: -48px; margin-left: 58px;">
-                    <h6><a href=""></a></h6>
-                    {{index}} | {{ tweet.content }}
+                    <h5><a href="">{{tweet.user.slug}}</a></h5>
+                    <a>{{ tweet.content }}</a>
                 </div>
             </div>
             <div class="panel-footer">
-                <div class="row float-right">
-                    <div>
+                <div class="row">
+                    <div class="col text-right float-right " style="padding-right: 0">
                         <a v-if="deleteConfirmation==tweet.id">Are you sure:
                             <a href="#" @click="deleteConfirmation=null;
                             postDeleteTweet({index: index, id: tweet.id})">Yes</a>
@@ -23,12 +23,11 @@
                         </a>
                         <a v-if="!deleteConfirmation  && tweet.user.id==currentUser.id" @click="deleteTweet(tweet.id)">delete</a>
                     </div>
-                    <div class="ml-2">
-                        created: {{ tweet.created_at | moment("from", "now")}}
+                    <div class="col-4 float-right" style="padding-left: 10px">
+                        posted: {{ tweet.created_at | moment("from", "now")}}
                     </div>
                 </div>
             </div>
-            <br>
         </div>
     </div>
 </template>
@@ -38,14 +37,14 @@
     import {sync} from 'vuex-pathify';
 
     export default {
-        data(){
-            return{
+        data() {
+            return {
                 deleteConfirmation: null,
             }
         },
-        methods:{
+        methods: {
             ...mapActions(['postDeleteTweet']),
-            deleteTweet(id){
+            deleteTweet(id) {
                 this.deleteConfirmation = id;
             }
         },
