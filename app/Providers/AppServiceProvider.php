ffe98cc22b1596848441eb99d6116105d8180e9f
Martin C <vinche1111@gmail.com>
2019-01-02 07:23:02 -0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //force to serve on https
+        \URL::forceScheme('https');
     }
 
     /**
